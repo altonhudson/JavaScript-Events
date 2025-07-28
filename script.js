9605a877c1b74ea19acc819166ab5352cc0bdb86
altonhudson <ah_1014@hotmail.com>
2025-07-28 19:05:37 -0400

    Form Events

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ let keyPressed = document.querySelector(".key-pressed")
 keyboardEvents.addEventListener('keydown', function(e) {
     keyPressed.innerHTML = e.key;
 }) 
+
+let formButton = document.querySelector('.form-button')
+let userName = document.querySelector('.user-name')
+let password = document.querySelector('.password')
+
+formButton.addEventListener("click", function(e) {
+    e.preventDefault()
+    alert("Your form has been successfully submitted.")
+})
