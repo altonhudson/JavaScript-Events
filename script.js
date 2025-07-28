c4877f54a5897e44bca63fcfc948710a2991f997
altonhudson <ah_1014@hotmail.com>
2025-07-28 18:57:06 -0400

    Keyboard event listener

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ mouseEvents.addEventListener("mouseover", function(e) {
 mouseEvents.addEventListener("mouseout", function(e) {
     mouseEvents.innerHTML = "Mouse Events"
 })
+
+let keyboardEvents = document.querySelector('.keyboard-events')
+let keyPressed = document.querySelector(".key-pressed")
+
+keyboardEvents.addEventListener('keydown', function(e) {
+    keyPressed.innerHTML = e.key;
+}) 
